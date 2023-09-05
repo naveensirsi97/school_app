64f4e48d15bc376dfb11b4d268c48f9940a82107
Nitin <nsheoran2005@gmail.com>
2023-09-05 15:10:55 +0530

    create DatabaseHelper

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,5 +1,6 @@
 import 'package:path/path.dart';
 import 'package:school_app/teacher/model/teacher_model.dart';
+import 'package:school_app/teacher/subject/model/subject_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
@@ -27,6 +28,7 @@ class DatabaseHelper {
         await openDatabase(dbPath, version: 1, onCreate: (database, version) {
       database.execute(
           'create table $teacherTable($id int primary key,$teacherName text,$salary int)');
+      database.execute('create table $subjectTable($subjectId int primary key,$subjectName text)');
       print("Table created successfully");
     });
   }
@@ -69,5 +71,45 @@ class DatabaseHelper {
       teacher.id,
     ]);
     print("update teacher successfully");
+  }
+
+  static Future addSubjectData(Subject subject) async {
+    await database.rawInsert(
+      'insert into $subjectTable values(?,?)',
+      [
+        subject.subjectId,
+        subject.subjectName,
+
+      ],
+    );
+    print('Data inserted successfully');
+  }
+
+  static Future<List<Subject>> getSubjectData() async {
+    List<Map<String, dynamic>> mapList =
+    await database.rawQuery('select * from $subjectTable');
+
+    List<Subject> subList = [];
+    for (int i = 0; i < mapList.length; i++) {
+      Map<String, dynamic> map = mapList[i];
+      Subject subjectData = Subject.fromMap(map);
+      subList.add(subjectData);
+    }
+    return subList;
+  }
+
+  static Future deleteSubject(int idNo) async {
+    await database.rawDelete("delete from $subjectTable where $subjectId = $idNo");
+    print("delete subject successfully");
+  }
+
+  static Future updateSubject(Subject subject) async {
+    await database.rawUpdate(
+        "update $subjectTable set $subjectName=? where $subjectId=?", [
+    subject.subjectId,
+     subject.subjectName
+
+    ]);
+    print("update subject successfully");
   }
 }
