124b04fbbbc247238ba2ba62107b6f0f7c8b3c41
Nilesh Sheoran <nksheoran2002@gmail.com>
2023-09-05 12:00:38 +0530

    implement database helper

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,4 +1,5 @@
 import 'package:path/path.dart';
+import 'package:school_app/student/model/student_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
@@ -25,6 +26,27 @@ class DatabaseHelper {
     String path = await getDatabasesPath();
     String dbPath = join(path, dbName);
     database = await openDatabase(dbPath,
-        version: 1, onCreate: (database, version) {});
+        version: 1, onCreate: (database, version) {
+      database.execute("create table $studentTable($rollNo int primary key,$studentName text,$grade int,$address text)");
+      print('Table create successfully');
+        });
+  }
+  static Future addStudent(Student student)async{
+   await database.rawInsert('insert into $studentTable values(?,?,?,?)',
+     [student.rollNo,student.studentName,student.grade,student.address]
+    );
+    print('Data inserted successfully');
+  }
+  static Future<List<Student>>
+  getStudentData()async{
+    List<Map<String,dynamic>> mapList=await database.rawQuery('select * from $studentTable');
+
+    List<Student> stdList=[];
+    for(int i=0;i<mapList.length;i++){
+      Map<String,dynamic> map=mapList[i];
+      Student student=Student.fromMap(map);
+      stdList.add(student);
+    }
+    return stdList;
   }
 }
