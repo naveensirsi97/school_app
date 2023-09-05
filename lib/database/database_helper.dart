c563806822fed9db96ac2b5b43de2da208e05b59
Nitin <nsheoran2005@gmail.com>
2023-09-05 12:11:16 +0530

    create database Helper

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,4 +1,5 @@
 import 'package:path/path.dart';
+import 'package:school_app/teacher/model/teacher_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
@@ -6,9 +7,7 @@ class DatabaseHelper {
   static String teacherTable = 'Teacher';
   static String id = 'Id';
   static String teacherName = 'Name';
-  static String subject = 'Subject';
   static String salary = 'Salary';
-  static String imgPath = 'Path';
 
   static String studentTable = 'Student';
   static String rollNo = 'RollNo';
@@ -26,6 +25,49 @@ class DatabaseHelper {
     String dbPath = join(path, dbName);
     database =
         await openDatabase(dbPath, version: 1, onCreate: (database, version) {
+      database.execute(
+          'create table $teacherTable($id int primary key,$teacherName text,$salary int)');
+      print("Table created successfully");
     });
+  }
+
+  static Future addPatientData(Teacher teacher) async {
+    await database.rawInsert(
+      'insert into $teacherTable values(?,?,?)',
+      [
+        teacher.id,
+        teacher.teacherName,
+        teacher.salary,
+      ],
+    );
+    print('Data inserted successfully');
+  }
+
+  static Future<List<Teacher>> getPatientData() async {
+    List<Map<String, dynamic>> mapList =
+        await database.rawQuery('select * from $teacherTable');
+
+    List<Teacher> patList = [];
+    for (int i = 0; i < mapList.length; i++) {
+      Map<String, dynamic> map = mapList[i];
+      Teacher patientData = Teacher.fromMap(map);
+      patList.add(patientData);
+    }
+    return patList;
+  }
+
+  static Future deletePatient(int idNo) async {
+    await database.rawDelete("delete from $teacherTable where $id = $idNo");
+    print("delete teacher successfully");
+  }
+
+  static Future updatePatient(Teacher teacher) async {
+    await database.rawUpdate(
+        "update $teacherTable set $teacherName=?,$salary=? where $id=?", [
+      teacher.teacherName,
+      teacher.salary,
+      teacher.id,
+    ]);
+    print("update teacher successfully");
   }
 }
