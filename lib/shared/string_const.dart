d19eea914b0edcf4cb620d78ca09fa79811c04ae
Nitin <nsheoran2005@gmail.com>
2023-09-05 14:15:59 +0530

    create String Const

--- lib/shared/string_const.dart
+++ lib/shared/string_const.dart
@@ -1,1 +1,6 @@
-class StringConstant {}
+class StringConstant {
+
+  static const String addTeacherText = 'Add Teacher Detail';
+  static const String showTeacherText = 'Show Teacher Detail';
+  static const String updateTeacherText = 'Update Teacher Detail';
+}
