10ce728533270593c7a865bfa2c8c122408a861a
Nitin <nsheoran2005@gmail.com>
2023-09-05 14:17:32 +0530

    TeacherModel added name

--- lib/teacher/model/teacher_model.dart
+++ lib/teacher/model/teacher_model.dart
@@ -2,21 +2,18 @@ class Teacher {
   int id;
   String teacherName;
   int salary;
-  String subject;
 
   Teacher({
     required this.id,
     required this.teacherName,
     required this.salary,
-    required this.subject,
   });
 
   static Teacher fromMap(Map<String, dynamic> map) {
     return Teacher(
       id: map['Id'],
-      teacherName: map['TeacherName'],
+      teacherName: map['Name'],
       salary: map['Salary'],
-      subject: map['Subject'],
     );
   }
 
@@ -25,7 +22,6 @@ class Teacher {
       'Id': id,
       'Name': teacherName,
       'Salary': salary,
-      'Subject': subject,
     };
   }
 }
