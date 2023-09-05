998b8b34ec63ba8ceaec09b6fec6891d945c67f7
Nitin <nsheoran2005@gmail.com>
2023-09-05 14:29:48 +0530

    labelText added stringConst class

--- lib/shared/string_const.dart
+++ lib/shared/string_const.dart
@@ -3,4 +3,8 @@ class StringConstant {
   static const String addTeacherText = 'Add Teacher Detail';
   static const String showTeacherText = 'Show Teacher Detail';
   static const String updateTeacherText = 'Update Teacher Detail';
+
+  static const String labelText1 = 'ID';
+  static const String labelText2 = 'Name';
+  static const String labelText3 = 'Salary';
 }
